f063e1f22f5fad8d0a52b325298e9c407d69067f
Samshul Islam Shakil <samshuleeeweb@gmail.com>
2023-03-27 23:32:16 +0600

    update header

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className='products-container'>
+                <h2>Product Coming Here</h2>
+            </div>
+            <div className='cart-container'>
+                <h4>Order Summary</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
